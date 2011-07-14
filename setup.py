6de413d1e0685b2c7a3eb1d2f9876282e8e85e77
Max Zwiessele <maxz@tuebingen.mpg.de>
2011-07-14 13:52:29 +0200

    example location changed

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ standard_params = dict(name='gptwosample',
 setuptools.setup(
     install_requires = ['numpy','scipy'],
     packages=setuptools.find_packages('./'),#['gptwosample','examples'],
-    #package_data={'examples':['*.csv','*.py']},
-    #data_files=[('examples',find_example_files())],
+    package_data={'examples':['*.csv','*.py']},
+    data_files=[('examples',find_example_files())],
     **standard_params
     )
