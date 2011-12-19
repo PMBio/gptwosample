resolved bug of confounder script - used product instead of sum in covaraince matrix of covariance method

gptwosample/data/data_analysis.py
@@ -44,7 +44,7 @@ def plot_roc_curve(path_to_result,path_to_ground_truth,
     result_names = []
     result_results = []
     for gene_name,result in result_data.iteritems():
-        result_names.append(gene_name.upper())
+        result_names.append(gene_name)
         result_results.append(scipy.ndarray.flatten(result)[0])
     result_names_sorting = scipy.argsort(result_names)
     result_names=scipy.array(result_names)[result_names_sorting]
@@ -52,7 +52,7 @@ def plot_roc_curve(path_to_result,path_to_ground_truth,
     ground_truth_names = []
     ground_truth_results = []
     for gene_name,truth in ground_truth_data.iteritems():
-        ground_truth_names.append(gene_name.upper())
+        ground_truth_names.append(gene_name)
         ground_truth_results.append(scipy.ndarray.flatten(truth)[0])
     ground_truth_sorting = scipy.argsort(ground_truth_names)
     ground_truth_names=scipy.array(ground_truth_names)[ground_truth_sorting]