672aba2b3b65c3eca9c707034f96e956724494a9
Max Zwiessele <ibinbei@gmail.com>
2013-03-05 15:36:26 +0000

    plot_rocs.py done

--- gptwosample/develop/confounder_module_warwick/plot_rocs.py
+++ gptwosample/develop/confounder_module_warwick/plot_rocs.py
@@ -5,8 +5,13 @@ Created on Mar 5, 2013
 '''
 import os
 from gptwosample.data.data_analysis import plot_roc_curve
+import pylab
 
+root = 'warwick'
 
-for f in os.listdir('warwick'):
-    if f.is_file and f.basename.endswith("bayes"):
-        plot_roc_curve(f, "../../examples/ground_truth_random_genes.csv", label=f.basename.split("_")[0])
+for f in os.listdir(root):
+    if os.path.splitext(os.path.basename(f))[0].endswith("bayes"):
+        plot_roc_curve(os.path.join(root,f), "../../examples/ground_truth_random_genes.csv", label=os.path.basename(f).split("_")[0])
+        
+pylab.legend(loc=4)
+pylab.savefig("warwick/roc.pdf")
