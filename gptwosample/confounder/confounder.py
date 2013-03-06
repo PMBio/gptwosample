7d690ac348543dd62f04bb131dab94f0267e01b1
Max Zwiessele <ibinbei@gmail.com>
2013-03-06 17:02:09 +0000

    all

--- gptwosample/confounder/confounder.py
+++ gptwosample/confounder/confounder.py
@@ -7,7 +7,7 @@ Confounder Learning and Correction Module
 import numpy
 from multiprocessing import cpu_count
 from pygp.covar.linear import LinearCFISO, LinearCF
-from pygp.covar.combinators import SumCF
+from pygp.covar.combinators import SumCF, ProductCF
 from pygp.covar.se import SqexpCFARD
 from pygp.gp.gplvm import GPLVM
 from pygp.optimize.optimize_base import opt_hyper
@@ -61,12 +61,15 @@ class ConfounderTwoSample():
             self._lvm_covariance = lvm_covariance
         else:
             rt = self.r * self.t
-            self.X_s = numpy.zeros((self.n * rt, self.n * self.r))
-#            for i in xrange(self.n):self.X_s[i * rt:(i + 1) * rt, i * self.r : (i+1) * self.r] = .1
-            for i in xrange(self.n * self.r):self.X_s[i * self.t:(i + 1) * self.t, i] = 1
+            self.X_r = numpy.zeros((self.n * rt, self.n * self.r))
+            for i in xrange(self.n * self.r):self.X_r[i * self.t:(i + 1) * self.t, i] = 1
+            rep = LinearCFISO(dimension_indices=numpy.arange(1 + q, 1 + q + (self.n * self.r)))
+            self.X_s = numpy.zeros((self.n*rt, self.n))
+            for i in xrange(self.n):self.X_s[i * self.n, rt:(i + 1) * self.n * rt, i] = .1
+            sam = LinearCFISO(dimension_indices=numpy.arange(1 + q, 1 + q + (self.n * self.r)))
             self._lvm_covariance = SumCF([LinearCF(dimension_indices=numpy.arange(1, 1 + q)),
-                                          LinearCFISO(dimension_indices=numpy.arange(1 + q, 1 + q + (self.n * self.r))),
-                                          SqexpCFARD(dimension_indices=numpy.array([0])),
+                                          rep,sam,
+                                          ProductCF([sam,SqexpCFARD(dimension_indices=numpy.array([0]))]),
                                           BiasCF(dimension_indices=numpy.arange(0, 1 + q))])
 
         # if initial_hyperparameters is None:
