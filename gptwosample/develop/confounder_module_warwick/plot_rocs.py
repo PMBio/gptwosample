79c31a43a6dcadf532b2e21a69da4f68a7fbc0ac
Max Zwiessele <ibinbei@gmail.com>
2013-03-06 13:06:52 +0000

    roc legend best

--- gptwosample/develop/confounder_module_warwick/plot_rocs.py
+++ gptwosample/develop/confounder_module_warwick/plot_rocs.py
@@ -15,7 +15,7 @@ for f in os.listdir(root):
     if os.path.splitext(os.path.basename(f))[0].endswith("bayes"):
         plot_roc_curve(os.path.join(root,f), "../../examples/ground_truth_random_genes.csv", label=os.path.basename(f).split("_")[0])
         
-pylab.legend(loc=4)
+pylab.legend()
 pylab.xlim(0,.2)
 pylab.savefig(os.path.join(root, "roc.pdf"))
 
