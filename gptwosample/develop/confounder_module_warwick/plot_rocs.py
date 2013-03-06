172a469d70e3796005e5b60e8144e0928ec2479e
Max Zwiessele <ibinbei@gmail.com>
2013-03-06 09:19:51 +0000

    wrong import

--- gptwosample/develop/confounder_module_warwick/plot_rocs.py
+++ gptwosample/develop/confounder_module_warwick/plot_rocs.py
@@ -6,15 +6,20 @@ Created on Mar 5, 2013
 import os
 from gptwosample.data.data_analysis import plot_roc_curve
 import pylab
-from gptwosample.develop.confounder_module_warwick.warwick import finished
+import sys
 
 root = 'warwick'
 s = "plotting roc curves..."
-print s
+print s,
 for f in os.listdir(root):
     if os.path.splitext(os.path.basename(f))[0].endswith("bayes"):
         plot_roc_curve(os.path.join(root,f), "../../examples/ground_truth_random_genes.csv", label=os.path.basename(f).split("_")[0])
         
 pylab.legend(loc=4)
 pylab.savefig("warwick/roc.pdf")
-finished(s)
+
+try:
+    sys.stdout.write(s + " " + '\033[92m' + u"\u2713" + '\033[0m' + '            \n')
+except:
+    sys.stdout.write(s + " done             \n")
+sys.stdout.flush()
