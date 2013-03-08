3b6e1dbc8d56d9f13a8c264cf51600f370d46ed9
Max Zwiessele <ibinbei@gmail.com>
2013-03-08 15:40:16 +0000

    run script update

--- gptwosample/develop/confounder_module_warwick/warwick.sh
+++ gptwosample/develop/confounder_module_warwick/warwick.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-python warwick.py $1 data conf ../../examples/warwick_control.csv ../../examples/warwick_treatment.csv plot_confounder $* &
-python warwick.py $1 data conf sam $* &
-python warwick.py $1 data conf rep $* &
-python warwick.py $1 data noconf $* &
-python warwick.py $1 data ideal $* &
-python warwick.py $1 data raw $* &
+echo running scripts...
+mkdir -p $1
+python warwick.py $1 data conf ../../examples/warwick_control.csv ../../examples/warwick_treatment.csv plot_confounder $* > $1/conf_all.txt &
+python warwick.py $1 data conf sam plot_confounder $* > $1/conf_sam.txt &
+python warwick.py $1 data conf rep plot_confounder $* > $1/conf_rep.txt &
+python warwick.py $1 data noconf $* > $1/noconf.txt &
+python warwick.py $1 data ideal $* > $1/ideal.txt &
+python warwick.py $1 data raw $* > $1/raw.txt &
 wait
+echo finished, plotting roc curves
 python plot_rocs.py $1
