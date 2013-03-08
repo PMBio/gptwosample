4f405872804580213e20dc9fbc534e43dcee3653
Max Zwiessele <ibinbei@gmail.com>
2013-03-08 09:35:55 +0000

    summary

--- gptwosample/develop/confounder_module_warwick/warwick.sh
+++ gptwosample/develop/confounder_module_warwick/warwick.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-python warwick.py "$@"
+python warwick.py $1 data conf ../../examples/warwick_control.csv ../../examples/warwick_treatment.csv plot_confounder $* &
+python warwick.py $1 data conf sam $* &
+python warwick.py $1 data conf rep $* &
+python warwick.py $1 data noconf $* &
+python warwick.py $1 data ideal $* &
+python warwick.py $1 data raw $* &
+wait
+python plot_rocs.py $1
