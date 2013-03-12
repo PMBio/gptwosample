f628237fc448b84a933e9c971ca2ea4839b50ca6
Max Zwiessele <ibinbei@gmail.com>
2013-03-12 09:19:23 +0000

    color cycle

--- gptwosample/develop/confounder_module_warwick/plot_rocs.py
+++ gptwosample/develop/confounder_module_warwick/plot_rocs.py
@@ -10,11 +10,23 @@ import sys
 
 root = sys.argv[1]
 s = "plotting roc curves..."
+
+colors = [[1,0.585,1.067],
+[144,1.016,1.221],
+[224,0.712,1.153],
+[80,1.013,1.263],
+[276,0.753,1.127],
+[99,0.566,1.193],
+[187,0.87,1.205],
+[53,0.843,1.119],
+[109,0.981,1.231],
+[154,0.625,1.194]]
+
 print s,
-for f in os.listdir(root):
+for i, f in enumerate(os.listdir(root)):
     if os.path.splitext(os.path.basename(f))[0].endswith("bayes"):
         label = os.path.basename(f)[::-1].split("_",1)[1][::-1]
-        plot_roc_curve(os.path.join(root,f), "../../examples/ground_truth_random_genes.csv", label=label)
+        plot_roc_curve(os.path.join(root,f), "../../examples/ground_truth_random_genes.csv", label=label, color=colors[i])
         
 pylab.legend(loc=4)
 pylab.xlim(0,.2)
