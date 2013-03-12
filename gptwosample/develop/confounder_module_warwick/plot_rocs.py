2e91c7bf6de3964389f39c19f066c81ba2940a06
Max Zwiessele <ibinbei@gmail.com>
2013-03-12 09:24:02 +0000

    color cycle

--- gptwosample/develop/confounder_module_warwick/plot_rocs.py
+++ gptwosample/develop/confounder_module_warwick/plot_rocs.py
@@ -7,11 +7,12 @@ import os
 from gptwosample.data.data_analysis import plot_roc_curve
 import pylab
 import sys
+import itertools
 
 root = sys.argv[1]
 s = "plotting roc curves..."
 
-colors = [[1,0.585,1.067],
+colors = itertools.cycle([[1,0.585,1.067],
 [144,1.016,1.221],
 [224,0.712,1.153],
 [80,1.013,1.263],
@@ -20,12 +21,13 @@ colors = [[1,0.585,1.067],
 [187,0.87,1.205],
 [53,0.843,1.119],
 [109,0.981,1.231],
-[154,0.625,1.194]]
+[154,0.625,1.194]])
 
 print s,
-for i, f in enumerate([x for x in os.listdir(root) if os.path.splitext(os.path.basename(f))[0].endswith("bayes")]):
-    label = os.path.basename(f)[::-1].split("_",1)[1][::-1]
-    plot_roc_curve(os.path.join(root,f), "../../examples/ground_truth_random_genes.csv", label=label, color=colors[i%len(colors)])
+for f in os.listdir(root):
+    if os.path.splitext(os.path.basename(f))[0].endswith("bayes"):
+        label = os.path.basename(f)[::-1].split("_",1)[1][::-1]
+        plot_roc_curve(os.path.join(root,f), "../../examples/ground_truth_random_genes.csv", label=label, color=colors.next())
         
 pylab.legend(loc=4)
 pylab.xlim(0,.2)
