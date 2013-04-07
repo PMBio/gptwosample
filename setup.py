3d409942e39d487dbadbac942ebe4cb3b9c81bb7
Max Zwiessele <ibinbei@gmail.com>
2013-04-07 21:57:19 +0100

    last minute changes

--- setup.py
+++ setup.py
@@ -2,12 +2,8 @@
 # -*- coding: utf-8 -*-
 import setuptools,os
 
-__description__ = """
-Python package for time series comparison by Gaussian Processes
-===============================================================
-
-
-"""
+with open("README",'r') as r:
+    __description__ = str(r.read())
 
 def get_recursive_data_files(path):
     out = []
@@ -21,13 +17,14 @@ standard_params = dict(name='gptwosample',
       description = __description__,
       author = 'Max Zwießele, Oliver Stegle',
       author_email='ibinbei@gmail.com',
-      url='http://')
+      url='http://',
+      license='Apache v2.0')
 
 setuptools.setup(
-    install_requires = ['numpy','scipy','pygp >=1.1.0'],
+    install_requires = ['numpy','scipy','pygp >=1.1.0', 'matplotlib >=1.2'],
     packages = setuptools.find_packages('./'),#['gptwosample','examples'],
     package_data ={'gptwosample.examples':['*.csv','*.sh']},
-    data_files = [('',['README'])] + get_recursive_data_files('./doc'),
+    data_files = [('',['README', 'LICENSE'])] + get_recursive_data_files('./doc'),
         #[('tests/',['*.py'])],
     entry_points={
         'console_scripts': [
