a883a4ae4aad4c0321dbbc69fc24eb86f9e5b9f5
Max Zwiessele <ibinbei@gmail.com>
2013-04-07 21:03:33 +0100

    install done, doc done, setup done

--- setup.py
+++ setup.py
@@ -19,15 +19,21 @@ def get_recursive_data_files(path):
 standard_params = dict(name='gptwosample',
       version = '0.1.7a',
       description = __description__,
-      author = 'Max Zwießele, Oliver Stegle')
+      author = 'Max Zwießele, Oliver Stegle',
+      author_email='ibinbei@gmail.com',
+      url='http://')
 
 setuptools.setup(
     install_requires = ['numpy','scipy','pygp >=1.1.0'],
     packages = setuptools.find_packages('./'),#['gptwosample','examples'],
     package_data ={'gptwosample.examples':['*.csv','*.sh']},
-    data_files = [('',['README','gptwosample.py','__main__.py'])] + \
-        get_recursive_data_files('./doc') + \
-        [('tests/',['*.py'])],
-    test_suite='tests',
+    data_files = [('',['README'])] + get_recursive_data_files('./doc'),
+        #[('tests/',['*.py'])],
+    entry_points={
+        'console_scripts': [
+            'gptwosample=gptwosample.__main__:main',
+            ]
+        },
+    test_suite='gptwosample.tests',
     **standard_params
     )
